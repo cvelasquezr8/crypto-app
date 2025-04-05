8f3dd78ac0c3795351fcc9b34350b309556a3119
Carlos <cvelasquezr8@gmail.com>
2025-04-05 14:31:19 -0500

    feat: Add depency injection and create asset view.

---
+++ app/src/main/java/com/example/cryptoproject/CoinCapApp.kt
@@ -0,0 +1,8 @@
+package com.example.cryptoproject
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoinCapApp: Application() {
+}
--- app/src/main/java/com/example/cryptoproject/MainActivity.kt
+++ app/src/main/java/com/example/cryptoproject/MainActivity.kt
@@ -4,14 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.cryptoproject.models.Asset
 import com.example.cryptoproject.ui.theme.CryptoProjectTheme
+import com.example.cryptoproject.views.AssetRow
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +37,30 @@ class MainActivity : ComponentActivity() {
         setContent {
             CryptoProjectTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        AssetRow(
+                            Asset( id = "1",
+                                name = "Bitcoin",
+                                symbol = "BTC",
+                                price = "87800",
+                                percentage = 5.45
+                            )
+                        );
+                        Spacer(modifier = Modifier.size(16.dp))
+                        AssetRow(
+                            Asset( id = "2",
+                                name = "Ethereum",
+                                symbol = "ETH",
+                                price = "18900",
+                                percentage = -6.78
+                            )
+                        );
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoProjectTheme {
-        Greeting("Android")
     }
 }
